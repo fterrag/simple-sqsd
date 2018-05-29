82d07c0c0186c0927015324b8001604fd011626b
Frank Terragna <fterragna@gmail.com>
2018-05-29 16:25:10 -0400

    Only run goveralls on master

--- travis.sh
+++ travis.sh
@@ -4,9 +4,10 @@ docker build -t test --target builder . || exit 1
 
 # Run unit tests and publish test coverage to Coveralls.
 docker run test go test -v ./... || exit 1
-docker run test goveralls -repotoken $COVERALLS_TOKEN || exit 1
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
+    docker run test goveralls -repotoken $COVERALLS_TOKEN || exit 1
+
     docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASS || exit 1
     docker build -t fterrag/simple-sqsd:latest . || exit 1
     docker push fterrag/simple-sqsd:latest || exit 1

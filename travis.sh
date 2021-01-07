0775de88cc9ec2b30200057be4b77b0c521c3efe
Frank Terragna <fterragna@gmail.com>
2021-01-07 17:09:39 -0500

    Auth with Docker Hub

--- travis.sh
+++ travis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASS || exit 1
+
 docker build -t test --target builder . || exit 1
 
 # Run unit tests and publish test coverage to Coveralls.
@@ -8,7 +10,6 @@ docker run test go test -v ./... || exit 1
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
     docker run test goveralls -repotoken $COVERALLS_TOKEN || exit 1
 
-    docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASS || exit 1
     docker build -t fterrag/simple-sqsd:latest . || exit 1
     docker push fterrag/simple-sqsd:latest || exit 1
 fi
